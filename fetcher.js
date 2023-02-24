91a8c686dadccc67138de859c19d03a03e5f96cf
Shujie Ma <shujie1st@gmail.com>
2023-02-24 17:56:35 -0500

    Add edge cases: invalid file path or URL

--- fetcher.js
+++ fetcher.js
@@ -6,37 +6,42 @@ const readline = require('readline');
 let URL = process.argv[2];
 let localFilePath = process.argv[3];
 
-// create interface for input and output
-let rl = readline.createInterface(process.stdin, process.stdout);
 
 request(URL, (error, response, body) => {
-  console.log('error', error);
+  
+  // if URL results in error, print error and terminate app
+  if (error) {
+    console.log(error);
+    return;
+  }
+
   console.log('statusCode:', response && response.statusCode);
   console.log('body:', body);
 
-  // download the resources if local file path not exist
-  if (!fs.existsSync(localFilePath)) {
+  let wrtieFile = function() {
     fs.writeFile(localFilePath, body, (error) => {
+    // Print error with writing file (e.g., invalid flie path)
       if (error) {
         console.log(error);
         return;
       }
+    
       console.log(`Downloaded and saved ${fs.statSync(localFilePath).size} bytes to ${localFilePath}.`);
-    }
-    );
-    rl.close();
+    });
+  };
+
+  // download the resources if local file not exist
+  if (!fs.existsSync(localFilePath)) {
+    wrtieFile();
   } else {
+
+    // create interface for input and output
+    let rl = readline.createInterface(process.stdin, process.stdout);
+
     // if file path already exists, user will be asked to type in "Y" or "N" to overwrite file or skip
     rl.question("File already exists! Please type in 'Y' + 'Enter' to overwrite the file! Or type in 'N' + 'Enter' to exit.", (answer) => {
       if (answer === "Y") {
-        fs.writeFile(localFilePath, body, (error) => {
-          if (error) {
-            console.log(error);
-            return;
-          }
-          console.log(`Downloaded and saved ${fs.statSync(localFilePath).size} bytes to ${localFilePath}.`);
-        }
-        );
+        wrtieFile();
       } else if (answer === "N") {
         console.log(`No change to the original file ${localFilePath}!`);
       }
